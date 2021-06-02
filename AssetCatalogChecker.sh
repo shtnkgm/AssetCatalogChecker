622744cf744b9059bd390d2cdb35149eecc200c8
shtnkgm <shtnkgm@gmail.com>
2021-06-02 09:51:53 +0900

    Update AssetCatalogChecker.sh

--- AssetCatalogChecker.sh
+++ AssetCatalogChecker.sh
@@ -8,7 +8,7 @@ find $1 -name "*.imageset" -not -path "*/Carthage/*" -not -path "*/Pods/*" | \
   sed "s/\.imageset//" | \
   while read -r name; do
     # 精度と検索速度向上のため、検索対象の拡張子を限定する
-    grep -Er "\"$name\"|/$name\"" $1 --exclude-dir={Carthage,Pods,build,fastlane} --include='*.swift' --include='*.m' --include='*.xib' --include='*.storyboard' > /dev/null
+    grep -Er "\"$name\"|/$name\"" $1 --exclude-dir={Carthage,Pods,build,fastlane} --exclude='SwiftGen.swift' --include='*.swift' --include='*.m' --include='*.xib' --include='*.storyboard' > /dev/null
     if [ $? -eq 0 ]; then
       echo "==> ✅ \"$name\" found"
     else
